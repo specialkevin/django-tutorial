9e40eda73d9512343720763b62a12d564c799804
Kevin Harriss <special.kevin@gmail.com>
2011-12-08 17:16:53 -0600

    Added the ability to add choices when creating a new poll

--- tutorial/polls/admin.py
+++ tutorial/polls/admin.py
@@ -1,4 +1,22 @@
 from polls.models import Poll
+from polls.models import Choice
 from django.contrib import admin
 
-admin.site.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,  {'fields': ['question']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,      {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Poll, PollAdmin)
