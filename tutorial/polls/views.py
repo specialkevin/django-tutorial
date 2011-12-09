94fe6f0ab7ff69c327de51156591a4e6ec100f84
Kevin Harriss <special.kevin@gmail.com>
2011-12-09 16:20:30 -0600

    Updated the templates and views for results andvoting

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -2,7 +2,9 @@
 #from django.http import HttpResponse
 from polls.models import Poll
 from django.shortcuts import render_to_response, get_object_or_404
-from django.http import Http404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
 #from django.template import Context, loader
 
 def index(request):
@@ -11,10 +13,24 @@ def index(request):
     
 def detail(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/detail.html', {'poll': p})        
+    return render_to_response('polls/detail.html', {'poll': p},
+                               context_instance=RequestContext(request))        
     
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s" % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/results.html', {'poll': p})
     
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        #Redisplay the poll voting form
+        return render_to_response('polls/detail.html', {
+            'poll': p,
+            'error_message': "You didn't select a choice.",
+        }, context_instance=RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
